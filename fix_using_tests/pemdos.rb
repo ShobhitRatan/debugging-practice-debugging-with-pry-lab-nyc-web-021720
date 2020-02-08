cc70158ccc20a0e465ba8531a6605dbacb5894c1
Shobhit Ratan <shobhit.ratan@baruchmail.cuny.edu>
2020-02-08 21:05:58 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -2,10 +2,10 @@ def snake_it_up(string)
   newstr = string
   if string[0] == "s"
     10.times do
-      newstr += "s"
+      newstr = "s" + newstr 
     end
   else
   string
   end
-  return newstr 
+  return newstr
 end
